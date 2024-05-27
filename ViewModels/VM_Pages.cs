c375642bf8576c17a9e6b4fb0077e5b14f5e2977
nightSanches <galesavich@gmail.com>
2024-05-27 22:51:18 +0500

    Разработанная модель представления главного окна. Савичев

---
+++ ViewModels/VM_Pages.cs
@@ -0,0 +1,25 @@
+﻿using pr44savichev.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pr44savichev.ViewModels
+{
+    public class VM_Pages : Notification
+    {
+        public VM_Tasks vm_tasks = new VM_Tasks();
+
+        public VM_Pages() => MainWindow.init.frame.Navigate(new View.Main(vm_tasks));
+
+        public RealyCommand OnClose
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    MainWindow.init.Close();
+                });
+            }
+        }
+    }
+}
