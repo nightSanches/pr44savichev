28c21ab524250800e146f38a03366bdcdd109021
nightSanches <galesavich@gmail.com>
2024-05-27 22:35:20 +0500

    Созданный класс Config. Савичев

---
+++ Classes/Database/Config.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pr44savichev.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=127.0.0.1;uid=root;pwd=;database=***;";
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
