83442397604fdec06b35a54325408ca0d6a40652
nightSanches <galesavich@gmail.com>
2024-05-27 22:50:11 +0500

    Разработанный контекст данных для модели задач. Савичев

---
+++ Context/TasksContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pr44savichev.Context
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Models.Tasks> Tasks { get; set; }
+
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql(Classes.Database.Config.connection, Classes.Database.Config.version);
+    }
+}
